05e247d76ad27710b1cb041841231b8c9cd06a7b
Maciej Rybka <maciek.rybka2004@gmail.com>
2026-03-17 20:05:46 +0100

    feat: implement TSP random search algorithm

---
+++ include/algorithms/RandomSearch.h
@@ -0,0 +1,12 @@
+pragma once
+#include "ITspAlgorithm.h"
+
+class RandomSearch : public ITspAlgorithm {
+    private:
+	    int iterations; // Number of random solutions to generate
+
+    public:
+        RandomSearch(int maxIterations = 100000);
+
+        TspResult* solve(const TspMatrix& matrix) override;
+};
